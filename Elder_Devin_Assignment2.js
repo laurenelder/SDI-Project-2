c2c0414b7e093b045ab560dad0aedd8669cb49e9
Devin Lauren Elder <laurenelder@fullsail.edu>
2012-10-31 14:31:25 -0400

    Beginning template for Project 2.

---
+++ Elder_Devin_Assignment2.js
@@ -0,0 +1,36 @@
+// Devin "Lauren" Elder
+// SDI 1211
+// Project 2
+// "Date"
+
+// My Variables
+
+
+// Procedure
+
+
+// Boolean Function
+
+
+
+// Number Function
+
+
+
+// String Function
+
+
+
+// String Function
+
+
+
+// Array Function
+
+
+
+// My Outputs
+
+
+
+// Main Function
