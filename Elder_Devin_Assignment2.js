a533841d19fd730faae829b3c4ec78d89de80564
Devin Lauren Elder <laurenelder@fullsail.edu>
2012-11-01 17:26:39 -0400

    Current not working code with no errors.

--- Elder_Devin_Assignment2.js
+++ Elder_Devin_Assignment2.js
@@ -4,23 +4,45 @@
 // "Date"
 
 // My Variables
-var key = myProcedure
-var gasTank = myBoolean
-var battVolt = myNumber
-var compInput = myString
-var sparkPlug = myArray
+//var gasTank = myBoolean
+//var battVolt = myNumber
+//var compInput = myString
+//var sparkPlug = myArray
 
 // Procedure
-
-
+var keyTurns = function(turns) {
+	var key = - 1;
+		if (turns * key > 0) {
+			console.log("The ignition is not on and the car will not run.");
+		} else {
+			console.log("The ignition is turned on and the car is able to run.");
+		};
+keyTurns(turns)
+};
 
 // Boolean Function
-
-
+var fuel = function(h, g) {
+		if (h > 0, [g] > 0) {
+		var gasTank = "gas"
+		console.log("The engine is getting enough fuel to start.");
+	} else {
+		var gasTank = "no gas"
+		console.log("The engine is not getting fuel and therefor will not start.");
+	};
+// Input a 0 if the tank is empty or if gas is not getting to the engine.
+fuel(0, 0)
+};	
 
 // Number Function
-
-
+console.log("Charge the battery with a battery charger till it is fully charged.");
+var voltage = function(v) {
+	var maxVoltage = 15
+while ([v] < maxVoltage) {
+	console.log("The battery is still charging, please wait.")
+	voltage++};
+voltage(8)
+};
+console.log("The battery is now fully charged.")
 
 // String Function
 
